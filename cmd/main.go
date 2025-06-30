ff7656a0f583d50a22786c770eeb9eeee70ba471
CesarSotnas <devcarlos.cesar@gmail.com>
2025-06-30 20:58:16 -0300

    :hammer: add main config

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	r := router.SetupRouter()
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Println("Server running on port 8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Error while starting the srever: %v", err)
+	}
+}
